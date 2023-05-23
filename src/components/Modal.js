baad7e0820065227fabc263fdd859ebd8922626e
islam-ahsanul <ahsanul.islam@g.bracu.ac.bd>
2023-05-23 15:23:00 +0600

    modal close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,8 +3,14 @@ import React from 'react';
 export const Modal = (props) => {
   //   console.log('in the modal');
   //   console.log(props);
+
+  const handleClick = (e) => {
+    if (e.target.classList.contains('backdrop')) {
+      props.setSelectedImg(null);
+    }
+  };
   return (
-    <div className="backdrop">
+    <div className="backdrop" onClick={handleClick}>
       <img src={props.selectedImg} alt="Expanded pic" />
     </div>
   );
